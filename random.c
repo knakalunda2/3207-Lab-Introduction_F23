6a586d52f0126f04d056f8ac24c32133e9469a5b
Kevina N <tul14037@temple.edu>
2023-08-30 11:01:11 -0400

    Completed Assignment

--- random.c
+++ random.c
@@ -1,8 +1,14 @@
+#include "random.h"
+#include <stdlib.h>
 
-char randchar(){
+char randchar(void){
+  char x;
+  int min = 65;
+  int max = 90;
+ 
+  int num = (rand() % (max - min + 1)) + min;
+    
+  x = (char) num ;
 
-for(
-
-
-
+  return x;
 }
